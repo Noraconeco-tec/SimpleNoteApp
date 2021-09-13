fc81247615ee761771c3386814cfe6cd4782d4d2
Noraconeco-tec <noraconeco.tec@gmail.com>
2021-09-14 00:45:04 +0900

    Added simple note dependency in module.

--- app/src/main/java/jp/co/noraconeco/simplenoteapp/di/NoteModule.kt
+++ app/src/main/java/jp/co/noraconeco/simplenoteapp/di/NoteModule.kt
@@ -4,6 +4,8 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import jp.co.noraconeco.simplenoteapp.model.SimpleNote
+import jp.co.noraconeco.simplenoteapp.model.SimpleNoteImpl
 import jp.co.noraconeco.simplenoteapp.repository.note.InMemoryNoteRepository
 import jp.co.noraconeco.simplenoteapp.repository.note.NoteRepository
 
@@ -15,4 +17,9 @@ internal abstract class NoteModule {
     abstract fun bindNoteRepository(
         inMemoryNoteRepository: InMemoryNoteRepository
     ) : NoteRepository
+
+    @Binds
+    abstract fun bindSimpleApp(
+        simpleNoteImpl: SimpleNoteImpl
+    ): SimpleNote
 }
