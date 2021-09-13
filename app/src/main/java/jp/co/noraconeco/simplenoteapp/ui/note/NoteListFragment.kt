db02b99b05d46d6aea82cb6f8543319ee61744f1
Noraconeco-tec <noraconeco.tec@gmail.com>
2021-09-13 15:52:52 +0900

    Created note list fragment.

---
+++ app/src/main/java/jp/co/noraconeco/simplenoteapp/ui/note/NoteListFragment.kt
@@ -0,0 +1,32 @@
+package jp.co.noraconeco.simplenoteapp.ui.note
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import jp.co.noraconeco.simplenoteapp.R
+
+class NoteListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = NoteListFragment()
+    }
+
+    private lateinit var viewModel: NoteListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_note_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NoteListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
