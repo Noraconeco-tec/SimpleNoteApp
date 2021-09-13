ba2b6e8cab6771d9715089fedbcc843e829cf674
Noraconeco-tec <noraconeco.tec@gmail.com>
2021-09-13 15:57:09 +0900

    Changed to use view binding in note list.

--- app/src/main/java/jp/co/noraconeco/simplenoteapp/ui/note/NoteListFragment.kt
+++ app/src/main/java/jp/co/noraconeco/simplenoteapp/ui/note/NoteListFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import jp.co.noraconeco.simplenoteapp.R
+import jp.co.noraconeco.simplenoteapp.databinding.FragmentNoteListBinding
 
 class NoteListFragment : Fragment() {
 
@@ -15,11 +15,21 @@ class NoteListFragment : Fragment() {
     }
 
     private val viewModel: NoteListViewModel by viewModels()
+    private var _binding: FragmentNoteListBinding? = null
+    private val binding: FragmentNoteListBinding
+        get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_note_list, container, false)
+    ): View {
+        _binding = FragmentNoteListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
